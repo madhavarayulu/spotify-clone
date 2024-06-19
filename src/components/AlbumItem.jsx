b9b5f6f2f2f43cde5e57754320db403f4d8d2d24
Madhava Rayulu <100406503+madhavarayulu@users.noreply.github.com>
2024-06-19 20:05:31 +0530

    Update AlbumItem.jsx
    updated navigation path

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -6,7 +6,7 @@ const AlbumItem = ({ image, name, desc, id }) => {
 
   return (
     <div
-      onClick={() => navigate(`/album/${id}`)}
+      onClick={() => navigate(`/spotify-clone/album/${id}`)}
       className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]"
     >
       <img className="rounded" src={image} alt="" />
